b73d3b155d5962ceb264c7f6f157ec25b52dc194
Eschiclers <chicle@protonmail.com>
2025-05-03 04:19:52 +0100

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,34 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	app: {
+		head: {
+			title: 'Chiclelandia - Rust',
+			htmlAttrs: {
+				lang: 'es'
+			},
+			meta: [
+				{
+					name: 'viewport',
+					content: 'width=device-width, initial-scale=1'
+				},
+				{
+					name: 'description',
+					content: 'Chiclelandia. Comunidad de videjuegos en español. SCP: Secret Laboratory, Rust y más.'
+				},
+				{
+					name: 'theme-color',
+					content: '#ffffff'
+				}
+			],
+			link: [
+				{
+					rel: 'icon',
+					type: 'image/x-icon',
+					href: '/favicon.png'
+				}
+			],
+		}
+	},
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
